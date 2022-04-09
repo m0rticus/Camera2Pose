35c411583afcda12f46b285b3ecf5567ffbd262c
ejendret <81583127+ejendret@users.noreply.github.com>
2022-04-08 21:12:31 -0700

    Wrote function to parse landmark data and store in 2D float array.

--- pose_estimation/socket/Client.py
+++ pose_estimation/socket/Client.py
@@ -30,13 +30,15 @@ def client_program():
 # each coordinate for each landmark is sent to the server
 # all numbers are separated by space characters
 # lmList[0, ..., 32][x, y, z] -> x0 y0 z0 ... x32 y32 z32
-def intake_landmark(lmList):
+def send_landmark_data(lmList):
 	client_socket = CommunicationSocket()
 	message = ' '.join([(' '.join([str(coordinate) for coordinate in landmark])) for landmark in lmList]) # convert two dimensional list to a string
 	client_socket.sendMessage(message)
 	client_socket.destroy() # close the connection
 	
 if __name__ == '__main__':
-	# use list to test operation of intake_landmark function
-	test_list = [[82359.32 for x in range(2)] for y in range(32)]
-	intake_landmark(test_list)
+	# use list to test operation of send_landmark function
+	test_list = [[1 for x in range(3)] for y in range(33)]
+	print(len(test_list))
+	print(test_list[20])
+	send_landmark_data(test_list)
