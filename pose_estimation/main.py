32351b0e0ea3ac1e143e5139ce60eafda5a099d1
Jonathan <bobchu351@gmail.com>
2022-04-29 21:35:04 -0700

    increased pose estimation fps

--- pose_estimation/main.py
+++ pose_estimation/main.py
@@ -10,7 +10,7 @@ wCam, hCam = 1920, 1080
 
 class PoseDetector:
 
-    def __init__(self, mode=True, upBody=False, smooth=True, detectCon=0.5, trackCon=0.95):
+    def __init__(self, mode=False, upBody=False, smooth=True, detectCon=0.5, trackCon=0.95):
         self.mode = mode
         self.upBody = upBody
         self.smooth = smooth
@@ -19,7 +19,7 @@ class PoseDetector:
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, 1, self.smooth, False, False, self.detectCon, self.trackCon)
+        self.pose = self.mpPose.Pose(self.mode, 0, self.smooth, False, False, self.detectCon, self.trackCon)
 
     def findPose(self, img, draw=True):
         img.flags.writeable = False
@@ -46,64 +46,68 @@ class PoseDetector:
         return lmList
 
 class ThreadedCamera(object):
-    def __init__(self):
-        self.capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
+    def __init__(self, source = 0):
 
-        # FPS = 1/X
-        # X = desired FPS
-        self.FPS = 1/30
-        self.FPS_MS = int(self.FPS * 1000)
+        self.capture = cv2.VideoCapture(source, cv2.CAP_DSHOW)
+        self.capture.set(3, wCam)
+        self.capture.set(4, hCam)
+        self.capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-        # Start frame retrieval thread
-        self.thread = Thread(target=self.update, args=())
+        self.thread = Thread(target = self.update, args = ())
         self.thread.daemon = True
         self.thread.start()
+
+        self.status = False
+        self.frame = None
 
     def update(self):
         while True:
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
-            time.sleep(self.FPS)
 
-    def show_frame(self):
-        cv2.imshow('frame', self.frame)
-        cv2.waitKey(self.FPS_MS)
+    def grab_frame(self):
+        if self.status:
+            return self.frame
+        return None  
 
 def main():
     # initialize camera
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cap.set(3, wCam)
-    cap.set(4, hCam)
+    cap = ThreadedCamera()
+    # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    # cap.set(3, wCam)
+    # cap.set(4, hCam)
+    # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
     prevTime = 0
     detector = PoseDetector()
     socket_client = socketClient.CommunicationSocket()
 
     while True:
         # read in the image
-        success, img = cap.read()
-        img = detector.findPose(img)
+        img = cap.grab_frame()
+        if img is not None:
+            # success, img = cap.read()
+            img = detector.findPose(img)
 
-        # find a specific landmark and highlight
-        lmList = detector.findPosition(img)
+            # find a specific landmark and highlight
+            lmList = detector.findPosition(img)
 
-        if len(lmList) >= 32:
-            # print("x is" + str(lmList[13][0]))
-            # print("y is" + str(lmList[13][1]))
-            # print("z is" + str(lmList[13][2]))
-            # lmList.append(offsetList)
-            socket_client.send_landmark_data(lmList)
+            if len(lmList) >= 32:
+                # print("x is" + str(lmList[13][0]))
+                # print("y is" + str(lmList[13][1]))
+                # print("z is" + str(lmList[13][2]))
+                # lmList.append(offsetList)
+                socket_client.send_landmark_data(lmList)
 
-        # calculate fps
-        currTime = time.time()
-        fps = 1 / (currTime - prevTime)
-        prevTime = currTime
+            # calculate fps
+            currTime = time.time()
+            fps = 1 / (currTime - prevTime)
+            prevTime = currTime
 
-        print(fps)
-        # update frame
-        # cv2.putText(img, f'FPS: {int(fps)}', (40, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 3)
-        # cv2.imshow("Frame", img)
-        # cv2.waitKey(1)
+            print(fps)
+            # update frame
+            cv2.putText(img, f'FPS: {int(fps)}', (40, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 3)
+            cv2.imshow("Frame", img)
+        cv2.waitKey(1)
 
     socket_client.destroy()
 
